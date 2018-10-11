6369b4c4a440375c6fa40c62d6743c8934fb0cca
Eugene Dementiev <eugene@springload.co.nz>
2018-10-12 09:03:06 +1300

    Adds ECR logins

---
+++ cmd/ecrEndpoint.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/springload/ecs-tool/deploy"
+)
+
+// ecrEndpointCmd represents the ecrEndpoint command
+var ecrEndpointCmd = &cobra.Command{
+	Use:   "ecr-endpoint",
+	Short: "Outputs the ECR endpoint",
+	Long: `
+Prints the ECR endpoint, which is constructed as {account_number}.dkr.ecr.{region}.amazonaws.com
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := deploy.EcrEndpoint(
+			viper.GetString("profile"),
+		); err != nil {
+			log.Fatal(err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(ecrEndpointCmd)
+}
