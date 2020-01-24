f998af59361b9d792f48efc1f562f340fc473c9e
Eugene Dementiev <eugene@springload.co.nz>
2020-01-24 14:40:23 +1300

    add processor command to modify decrypted ejson content

--- cmd/ejson.go
+++ cmd/ejson.go
@@ -12,14 +12,23 @@ import (
 )
 
 var ejsonCmd = &cobra.Command{
-	Use:   "ejson [file]",
+	Use:   "ejson -- [processor command with arguments]",
 	Short: "decrypt ejson and push to SSM",
 	Long: `Decrypt the supplied ejson file and write to the
 specified parameter in SSM Parameter Store
+
+The processor commands should read the decrypted ejson file from stdin and should output
+the processed content to stdout.
 `,
-	Args: cobra.MaximumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		var kmsKey, parameterName, encryptedFile, privateKey, privateKeyDir string
+		var processor []string
+
+		if len(args) > 0 {
+			processor = args
+		} else {
+			processor = viper.GetStringSlice("ejson.processor")
+		}
 
 		parameterName = viper.GetString("ejson.name")
 		if parameterName == "" {
@@ -30,11 +39,7 @@ specified parameter in SSM Parameter Store
 			log.Fatal("please set the KMS key with --kms-key or -k")
 		}
 
-		if len(args) == 1 {
-			encryptedFile = args[0]
-		} else {
-			encryptedFile = viper.GetString("ejson.file")
-		}
+		encryptedFile = viper.GetString("ejson.file")
 		if encryptedFile == "" {
 			log.Fatal("please specify the ejson file to decrypt")
 		}
@@ -53,7 +58,7 @@ specified parameter in SSM Parameter Store
 		if err != nil {
 			log.WithError(err).Fatalf("can't decrypt the file %s", encryptedFile)
 		}
-		if err := lib.WriteSSMParameter(viper.GetString("profile"), parameterName, kmsKey, string(decryptedValue)); err != nil {
+		if err := lib.WriteSSMParameter(viper.GetString("profile"), parameterName, kmsKey, string(decryptedValue), processor); err != nil {
 			log.WithError(err).Fatal("can't write the ssm parameter")
 		}
 	},
@@ -61,12 +66,15 @@ specified parameter in SSM Parameter Store
 
 func init() {
 	rootCmd.AddCommand(ejsonCmd)
-	ejsonCmd.PersistentFlags().StringP("name", "n", "", "Name of the parameter to write to")
-	ejsonCmd.PersistentFlags().StringP("kms-key", "k", "", "Name of the kms key to use")
-	ejsonCmd.PersistentFlags().StringP("keyvar", "", "EJSON_PRIVATE", "name of the env variable with private key")
 	ejsonCmd.PersistentFlags().StringP("dirvar", "", "EJSON_KEYDIR", "name of the env variable that has the ejson private keys folder")
-	viper.BindPFlag("ejson.name", ejsonCmd.PersistentFlags().Lookup("name"))
-	viper.BindPFlag("ejson.keyvar", ejsonCmd.PersistentFlags().Lookup("keyvar"))
+	ejsonCmd.PersistentFlags().StringP("file", "f", "", "ejson file to decrypt")
+	ejsonCmd.PersistentFlags().StringP("name", "n", "", "name of the parameter to write to")
+	ejsonCmd.PersistentFlags().StringP("keyvar", "", "EJSON_PRIVATE", "name of the env variable with the private key")
+	ejsonCmd.PersistentFlags().StringP("kms-key", "k", "", "kms key Id / alias to use for SSM Parameter encryption")
+
 	viper.BindPFlag("ejson.dirvar", ejsonCmd.PersistentFlags().Lookup("dirvar"))
+	viper.BindPFlag("ejson.file", ejsonCmd.PersistentFlags().Lookup("file"))
+	viper.BindPFlag("ejson.keyvar", ejsonCmd.PersistentFlags().Lookup("keyvar"))
 	viper.BindPFlag("ejson.kms_key", ejsonCmd.PersistentFlags().Lookup("kms-key"))
+	viper.BindPFlag("ejson.name", ejsonCmd.PersistentFlags().Lookup("name"))
 }
--- lib/ssm.go
+++ lib/ssm.go
@@ -1,16 +1,32 @@
 package lib
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+
 	"github.com/apex/log"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ssm"
 )
 
-func WriteSSMParameter(profile, parameterName, kmsKey, value string) error {
+func WriteSSMParameter(profile, parameterName, kmsKey, value string, processor []string) error {
 	err := makeSession(profile)
 	if err != nil {
 		return err
 	}
+	// run the processor command, feed the value to its input and get the response
+	if len(processor) > 0 {
+		value, err = processInput(value, processor)
+		if err != nil {
+			return fmt.Errorf("processor command exited with an error: %s", err)
+		}
+	}
+
 	svc := ssm.New(localSession)
 	resp, err := svc.PutParameter(&ssm.PutParameterInput{
 		Name:      aws.String(parameterName),
@@ -25,4 +41,57 @@ func WriteSSMParameter(profile, parameterName, kmsKey, value string) error {
 	log.Infof("wrote parameter '%s' version %d", parameterName, aws.Int64Value(resp.Version))
 
 	return nil
+}
+
+func processInput(value string, command []string) (string, error) {
+	log.Debugf("running command '%s'", strings.Join(command, " "))
+	output := new(bytes.Buffer)
+
+	cmd := exec.Command(command[0], command[1:]...)
+
+	// get the stdin to write to
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return "", err
+	}
+	go func() {
+		defer stdin.Close()
+		io.WriteString(stdin, value)
+	}()
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return "", err
+	}
+	go func() {
+		defer stdout.Close()
+		io.Copy(output, stdout)
+	}()
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return "", err
+	}
+	go func() {
+		defer stderr.Close()
+		io.Copy(os.Stderr, stderr)
+	}()
+
+	if err := cmd.Start(); err != nil {
+		return "", err
+	}
+	if err := cmd.Wait(); err != nil {
+		return "", err
+	}
+
+	// check if we still have a valid JSON file
+	{
+		out := make(map[string]interface{})
+		err := json.Unmarshal(output.Bytes(), &out)
+		if err != nil {
+			return "", fmt.Errorf("got invalid json from the processor output: %s", err)
+		}
+	}
+
+	return output.String(), nil
 }
