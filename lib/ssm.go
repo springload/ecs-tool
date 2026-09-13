0963bb93cbddd5585342fd75df4ee554a657ba67
alexbell708 <alexbell610@gmail.com>
2026-09-14 09:52:53 +1200

    updated mergo module path for compatibility witb v1.0.0+

--- lib/ssm.go
+++ lib/ssm.go
@@ -14,7 +14,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
 	"github.com/aws/aws-sdk-go-v2/service/ssm/types"
-	"github.com/imdario/mergo"
+	"dario.cat/mergo"
 )
 
 func WriteSSMParameter(profile, parameterName, kmsKey, value string, processor []string, pickJsonKeys []string) error {
